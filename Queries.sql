c8145f774695aad22c0e549ebd102bdf304bc6b1
Dhananjay Kumar <dhananjay0850@gmail.com>
2026-05-30 23:47:51 +0530

    Updated schema queries, sample insert records, normalization document, and ERD diagram to perfectly match setup_db.sql

--- Queries.sql
+++ Queries.sql
@@ -1,54 +1,53 @@
-#What if a person changes there number permanently?
-update voter_table set Phone=9283478293 where AADHAAR="5773 7940 7366";
+-- # What if a person changes their phone number permanently?
+UPDATE voter_table SET Phone = 9283478293 WHERE Aadhaar = "577379407366";
 
 
-#What will you do if for some political reason voting needs to occur again and you have to delete all rows from the vote and result table?
-truncate result;
-truncate vote_table;
-select * from result;
-select * from vote_table;
+-- # What will you do if for some political reason voting needs to occur again and you have to delete all rows from the vote and result table?
+TRUNCATE result;
+TRUNCATE vote_table;
+SELECT * FROM result;
+SELECT * FROM vote_table;
 
 
-#Do you want to know how many people from voted from a particular district?
-CREATE VIEW district_vote_count AS
-SELECT DistrictID, SUM(Vote_Count)
-FROM Result
-GROUP BY DistrictID;
+-- # Do you want to know how many people voted from a particular district?
+CREATE OR REPLACE VIEW district_vote_count AS
+SELECT DistrictId, SUM(Vote_Count) AS 'Total_District_Votes'
+FROM result
+GROUP BY DistrictId;
 
-drop view district_vote_count;
-select* from district_vote_count;
+SELECT * FROM district_vote_count;
+DROP VIEW IF EXISTS district_vote_count;
 
 
-#Do you know how many a votes each party got from all the districts combined?
-CREATE VIEW party_vote_count AS
-SELECT Result.PartyID, party_table.PartyName,SUM(Vote_Count) AS 'Total_Count'
-FROM Result,party_table
-WHERE Party_table.Partyid = Result.partyID
-GROUP BY result.PartyID
+-- # Do you know how many votes each party got from all the districts combined?
+CREATE OR REPLACE VIEW party_vote_count AS
+SELECT r.PartyId, pt.PartyName, SUM(r.Vote_Count) AS 'Total_Count'
+FROM result r
+INNER JOIN party_table pt ON pt.PartyId = r.PartyId
+GROUP BY r.PartyId, pt.PartyName
 ORDER BY Total_Count DESC;
 
-
-drop view party_vote_count;
-select* from party_vote_count;
-
-
-#What will we do with a person’s information who dies? 
-delete voter_table,user_table from Voter_Table,User_Table where Voter_Table.Aadhaar=User_table.Aadhaar and User_Table.isActive=0;
-#OR 
-delete Voter_Table,User_Table From Voter_Table INNER JOIN User_Table on Voter_Table.Aadhaar=User_table.Aadhaar where User_Table.isActive=0;
-
-select count(*) from user_table where User_Table.isActive=0;
-insert into User_Table values("UDJ092345","UDJ@0192",0,"8392 3921 0192",2);
-insert into Voter_Table values("8392 3921 0192","Aditi","Kharat","Simran","Jitendra","F","1948-06-06",72,234,9821009201);
+SELECT * FROM party_vote_count;
+DROP VIEW IF EXISTS party_vote_count;
 
 
-#Want all the details related to each candidate?
-create view candidate_detail as
-SELECT CandidateID, party_table.PartyName, voter_table.*
-FROM candidate_table LEFT JOIN voter_table 
-ON candidate_table.AADHAAR = voter_table.AADHAAR
-INNER JOIN party_table 
-ON Party_table.Partyid = candidate_table.partyID;
+-- # What will we do with a person's information who is inactive / deceased?
+-- # Safe lookup to count inactive voters
+SELECT COUNT(*) FROM user_table WHERE IsActive = FALSE;
 
-drop view candidate_detail;
-select * from candidate_detail;
+-- # Clean deletion of inactive voter records using secure ANSI joins
+DELETE vt, ut 
+FROM voter_table vt 
+INNER JOIN user_table ut ON vt.Aadhaar = ut.Aadhaar 
+WHERE ut.IsActive = FALSE;
+
+
+-- # Want all the details related to each candidate?
+CREATE OR REPLACE VIEW candidate_detail AS
+SELECT ct.CandidateId, pt.PartyName, vt.Aadhaar, vt.FirstName, vt.MiddleName, vt.LastName, vt.Sex, vt.Birthday, vt.Age, vt.Phone, vt.Email, ct.DistrictId
+FROM candidate_table ct 
+LEFT JOIN voter_table vt ON ct.Aadhaar = vt.Aadhaar
+INNER JOIN party_table pt ON pt.PartyId = ct.PartyId;
+
+SELECT * FROM candidate_detail;
+DROP VIEW IF EXISTS candidate_detail;
